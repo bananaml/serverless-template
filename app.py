6d9f8e6f024596e8a7ee6e6647b9c4d47a2aa12f
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2023-02-04 16:07:31 -0800

    replace bert with mpnet

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-from transformers import pipeline
-import torch
+from sentence_transformers import SentenceTransformer
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = SentenceTransformer("sentence-transformers/paraphrase-mpnet-base-v2")
+
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
+    prompt = model_inputs.get("prompt", None)
     if prompt == None:
-        return {'message': "No prompt provided"}
-    
+        return {"message": "No prompt provided"}
+
     # Run the model
     result = model(prompt)
 
