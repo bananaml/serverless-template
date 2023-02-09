31566659bc48f69dc8e66688de8a5fae6dce952c
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2023-02-08 20:02:54 -0800

    remove unecessary argument to pipeline in download.py

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from transformers import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='sentence-transformers/paraphrase-mpnet-base-v2')
+    pipeline(model='sentence-transformers/paraphrase-mpnet-base-v2')
 
 if __name__ == "__main__":
     download_model()
