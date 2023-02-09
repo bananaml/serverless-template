4a35620b12e9efd69fc557ff1f1b66e71fb6ee2d
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2023-02-08 20:12:25 -0800

    doh

--- download.py
+++ download.py
@@ -3,11 +3,11 @@
 
 # In this example: A Huggingface BERT model
 
-from transformers import pipeline
+from sentence_transformers import SentenceTransformer
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline(model='sentence-transformers/paraphrase-mpnet-base-v2')
+    SentenceTransformer('sentence-transformers/paraphrase-mpnet-base-v2')
 
 if __name__ == "__main__":
     download_model()
